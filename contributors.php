617250c6698d77a810fb054536af719143be36c2
dkolovos <dkolovos>
2007-01-19 15:21:34 +0000

    *** empty log message ***

---
+++ contributors.php
@@ -0,0 +1,56 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+
+	#*****************************************************************************
+	#
+	# template.php
+	#
+	# Author: 		Freddy Allilaire
+	# Date:			2006-05-29
+	#
+	# Description: Type your page comments here - these are not sent to the browser
+	#
+	#
+	#****************************************************************************
+	
+	#
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "Contributors/Interested parties to Epsilon";
+	$pageKeywords	= "";
+	$pageAuthor		= "Dimitrios Kolovos";
+	
+	# Paste your HTML content between the EOHTML markers!	
+	$html = <<<EOHTML
+
+<!-- Main part -->
+	<div id="midcolumn">
+		<table width="100%">
+			<tr>
+				<td width="50%">
+					<h1>$pageTitle</h1>
+				</td>
+				<td align="right">
+					<img alt="Epsilon Logo" src="resources/epsilonlogo.png" valign="top" />
+				</td>
+			</tr>
+		</table>
+
+		<h3>Contributors</h3>
+		
+		This paige contains the contributors and interested parties to Epsilon.<br><br>
+		
+		<ul>
+		
+		<li>
+			<a href="http://www.cs.york.ac.uk">Department of Computer Science, The University of York</a>
+		</ul>
+		
+		<hr class="clearer" />
+
+	</div>
+
+EOHTML;
+
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
