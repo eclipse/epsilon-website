82c841406ed8dc67bf3024049e35b94fd19e1dab
dkolovos <dkolovos>
2007-01-27 02:59:22 +0000

    *** empty log message ***

--- test.php
+++ test.php
@@ -1,3 +1,14 @@
 <?='Started1...'?>
-<?=file_get_contents('http://dev.eclipse.org/viewcvs/indextech.cgi/org.eclipse.gmt/epsilon/org.epsilon.eclipse.help/index.html')?>
-<?='Finished...'?>
+<?
+//$fp = fopen('http://dev.eclipse.org/viewcvs/indextech.cgi/org.eclipse.gmt/epsilon/org.epsilon.eclipse.help/index.html','r');
+$fp = fopen('http://www.google.com','r');
+$content = '';
+//keep reading until there's nothing left
+while ($line = fread($fp, 1024)) {
+	$content .= $line;
+?>
+<?=$content?>
+<?
+}
+?>
+}<?='Finished...'?>
