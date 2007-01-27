a0b9ac9dc93f5015d359bfa5509e59747191a4dd
dkolovos <dkolovos>
2007-01-27 12:17:15 +0000

    *** empty log message ***

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
-<?='Started...'?>
+<?='Started#1...'?>
 <?
-$fp = fopen('http://dev.eclipse.org/viewcvs/indextech.cgi/org.eclipse.gmt/epsilon/org.epsilon.eclipse.help/index.html','r');
+$fp = fopen('http://dev.eclipse.org/viewcvs/indextech.cgi/org.eclipse.gmt/epsilon/examples/OO2DB/readme.txt','r');
 //$fp = fopen('http://www.google.com','r');
 $content = '';
 //keep reading until there's nothing left
