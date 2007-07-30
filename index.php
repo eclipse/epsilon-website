31b7196ec2b7d752b7127f97562f55035b10632a
dkolovos <dkolovos>
2007-07-30 13:43:46 +0000

    *** empty log message ***

--- index.php
+++ index.php
@@ -48,7 +48,7 @@
 				      	etc. It will provide a metamodel-agnostic approach that supports model management tasks for any kind of metamodel and model 
 				      	instances.
 -->
-				      	<br /><br /><a href="about.php">more about Epsilon &raquo;</a>
+				      	<br /><br /><a href="about.php">more about Epsilon &raquo;</a>, <br /><br /><a href="cinema/">watch Flash demos &raquo;</a>
 				      </p>
 		  		</td>
 				<td align="right">
