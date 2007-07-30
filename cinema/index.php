5c898fcdc29f33f2bcd3f5574053dcb6a6f113e7
dkolovos <dkolovos>
2007-07-30 11:59:14 +0000

    *** empty log message ***

---
+++ cinema/index.php
@@ -0,0 +1,51 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+
+	#*****************************************************************************
+	#
+	# template.php
+	#
+	# Author: 		Freddy Allilaire
+	# Date:			2006-05-29
+	#
+	# Description: Type your page comments here - these are not sent to the browser
+	#
+	#
+	#****************************************************************************
+	
+	#
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "Epsilon Cinema";
+	$pageKeywords	= "";
+	$pageAuthor		= "Freddy Allilaire";
+	
+	# Paste your HTML content between the EOHTML markers!	
+	$html = <<<EOHTML
+
+<!-- Main part -->
+	<div id="midcolumn">
+		<table width="100%">
+			<tr>
+				<td width="50%">
+					<h1>$pageTitle</h1>
+				</td>
+				<td align="right">
+					<img alt="Epsilon Logo" src="../resources/epsilonlogo.png" valign="top" />
+				</td>
+			</tr>
+		</table>
+
+		<h3>Cinema</h3>
+		
+		<ul>
+		<li><a href="BuildOOInstance.part1.swf">Building an OO model (Part 1)</a> In the first part of this
+		example, we define a new metamodel using Emfatic and instantiate it using Exeed.
+		</ul>
+      	<hr class="clearer" />
+	</div>
+
+EOHTML;
+
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
