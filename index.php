5e5aa66fef2158c4403df302b0f73977c4dd5813
dkolovos <dkolovos>
2007-08-10 23:39:16 +0000

    *** empty log message ***

--- index.php
+++ index.php
@@ -204,6 +204,13 @@
 	<script type="text/javascript" language="javascript" src="http://www.statcounter.com/counter/counter.js"></script><noscript><a href="http://www.statcounter.com/" target="_blank"><img  src="http://c6.statcounter.com/counter.php?sc_project=2185757&java=0&security=2d5ff082&invisible=1" alt="free web hit counter" border="0"></a> </noscript>
 	<!-- End of StatCounter Code -->
 
+	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+	</script>
+	<script type="text/javascript">
+	_uacct = "UA-1498421-2";
+	urchinTracker();
+	</script>
+
 EOHTML;
 
 
