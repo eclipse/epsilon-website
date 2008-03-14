d0ad12036bb2290c4d34f52dc4a95f61410963fd
dkolovos <dkolovos>
2008-03-14 16:47:02 +0000

    *** empty log message ***

--- download.php
+++ download.php
@@ -17,7 +17,7 @@
 	$pageTitle 		= "Download Epsilon";
 	$pageKeywords	= "";
 	$pageAuthor		= "Dimitrios Kolovos";
-	$version = "1.3.0";
+	$version = "1.3.2";
 	
 	# Paste your HTML content between the EOHTML markers!	
 	$html = <<<EOHTML
