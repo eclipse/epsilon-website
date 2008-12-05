dbdb282e6ec8f8d83f536b28ba3694ed8a67d072
dkolovos <dkolovos>
2008-12-05 09:26:37 +0000

    *** empty log message ***

--- index.new.php
+++ index.new.php
@@ -69,10 +69,10 @@
 			<tr>
 				<td width="80%">
 					<h1>$pageTitle</h1>
-					<h3>Welcome</h3>
+					<!--h3>Welcome</h3-->
 				      <p>
 						Epsilon is a metamodel-agnostic component that supports model navigation, 
-						creation, and modification operations <!--a href="about.new.php">(more...)</a-->.
+						creation, and modification operations<!--a href="about.new.php">(more...)</a-->.
 				      </p>
 		  		</td>
 				<td align="right">
