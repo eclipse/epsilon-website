f79b3a0d46455196c32035203ee45117520eb67a
dkolovos <dkolovos>
2009-02-10 10:48:25 +0000

    *** empty log message ***

---
+++ usedin.php
@@ -0,0 +1,60 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+
+	#*****************************************************************************
+	#
+	# template.php
+	#
+	# Author: 		Freddy Allilaire
+	# Date:			2006-05-29
+	#
+	# Description: Type your page comments here - these are not sent to the browser
+	#
+	#
+	#****************************************************************************
+	
+	#
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "Used in...";
+	$pageKeywords	= "";
+	$pageAuthor		= "Dimitrios Kolovos";
+	
+	# Paste your HTML content between the EOHTML markers!	
+	$html = <<<EOHTML
+
+<!-- Main part -->
+	<div id="midcolumn">
+		<table width="100%">
+			<tr>
+				<td width="50%">
+					<h1>$pageTitle</h1>
+				</td>
+				<td align="right">
+					<img alt="Epsilon Logo" src="resources/epsilonlogo.png" valign="top" />
+				</td>
+			</tr>
+		</table>
+		
+		We have assembled the list of organizations where Epsilon is
+		being used through the posts in the eclipse.epsilon and eclipse.technology.gmt newsgroups
+		and through personal communication. So, for each organization that appears
+		in the list, to the best of our knowledge, at least one person there uses 
+		some aspect(s) of Epsilon. This does not imply that the organization itself 
+		endorses Epsilon.
+		
+		If you are using Epsilon and you would
+		like your organization to be included in the list of users, please let us know
+		through the newsgroup. Similarly, if your organization is 
+		currently in the list of users and you would like it to be removed 
+		(e.g. Epsilon is not used there any more) please also let us know and we
+		will see to your request as soon as possible.
+		
+		<hr class="clearer" />
+
+	</div>
+
+EOHTML;
+
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
