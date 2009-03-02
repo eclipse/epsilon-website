c4fb0dad26dd2a81a8997be2a39ceb2c765bec53
dkolovos <dkolovos>
2009-03-02 14:41:50 +0000

    *** empty log message ***

--- _projectCommon.php
+++ _projectCommon.php
@@ -30,6 +30,7 @@ EOBRANDING;
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
 	# these are optional
 	$Nav->addNavSeparator("Epsilon", "/gmt/epsilon/");
+	$Nav->addCustomNav("Home", "/gmt/epsilon/", "_self", 1);
 	$Nav->addCustomNav("Download", "/gmt/epsilon/download.php", "_self", 1);
 	$Nav->addCustomNav("Source code", "http://dev.eclipse.org/viewsvn/index.cgi/?root=Modeling_EPSILON", "_self", 1);
 	$Nav->addNavSeparator("Documentation", "#");
