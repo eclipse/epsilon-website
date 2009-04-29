4f423068906598ef3c4d631a27d82b42fde40ab7
dkolovos <dkolovos>
2009-04-29 14:03:45 +0000

    *** empty log message ***

--- _projectCommon.php
+++ _projectCommon.php
@@ -45,6 +45,8 @@ EOBRANDING;
 	$Nav->addCustomNav("Newsgroup", "news://news.eclipse.org/eclipse.epsilon", "_self", 1);
 	$Nav->addCustomNav("Web interface", "http://www.eclipse.org/newsportal/thread.php?group=eclipse.epsilon", "_self", 1);
 	$Nav->addCustomNav("Instructions", "http://wiki.eclipse.org/index.php/Webmaster_FAQ#How_do_I_access_the_Eclipse_newsgroups.3F", "_self", 2);
+	$Nav->addNavSeparator("Giving back", "#");
+	$Nav->addCustomNav("Spread the word", "/gmt/epsilon/spreadtheword.php", "_self", 1);
 	$Nav->addNavSeparator("Bugzilla", "#");
 	$Nav->addCustomNav("View bugs", "https://bugs.eclipse.org/bugs/buglist.cgi?product=gmt&component=Epsilon&cmdtype=doit&order=Reuse+same+sort+as+last+time", "_self", 1);
 	$Nav->addCustomNav("Report a bug", "https://bugs.eclipse.org/bugs/enter_bug.cgi?product=GMT&component=Epsilon", "_self", 1);	
---
+++ spreadtheword.php
@@ -0,0 +1,80 @@
+<?php  																														
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	
+$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+
+	#*****************************************************************************
+	#
+	# template.php
+	#
+	# Author: 		Freddy Allilaire
+	# Date:			2006-05-29
+	#
+	# Description: Type your page comments here - these are not sent to the browser
+	#
+	#
+	#****************************************************************************
+	
+	#
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "Spread the word";
+	$pageKeywords	= "";
+	$pageAuthor		= "Dimitrios Kolovos";
+	include ('common.php');
+	ob_start();
+?>
+
+	<div id="midcolumn">
+
+		<h3>Rate Epsilon at Eclipse Plugin Central</h3>
+		
+		<div style="float:right">
+			<form method="post" action="http://www.eclipseplugincentral.com/Web_Links.html">
+
+			<select name="rating" style="width:67px">
+			<option selected>--</option>
+			<option>10</option>
+			<option>9</option>
+			<option>8</option>
+			<option>7</option>
+			<option>6</option>
+			<option>5</option>
+			<option>4</option>
+			<option>3</option>
+			<option>2</option>
+			<option>1</option>
+			</select>
+			<br>
+			<input type="hidden" name="ratinglid" value="842">
+			<input type="hidden" name="ratinguser" value="outside">
+			<input type="hidden" name="req" value="addrating">
+			<input type="submit" value="Vote!" style="width:67px; margin-top:5px">
+			</form>
+		</div>
+		<p>Eclipse Plugin Central is the most comprehensive source of Eclipse plugins. You can use the form on the right to rate Epsilon directly from this page without needing to have an account, login or anything (but please vote only once :)).</p>
+		
+		<h3>Add Epsilon to your Ohloh stack</h3>
+		<div style="float:right">
+		<script type="text/javascript" src="http://www.ohloh.net/p/8615/widgets/project_users.js?style=blue"></script>
+		</div>
+		<p>Ohloh is an increasingly popular social networking site that connects software with the people that develop and use it. You can click on the widget on the right to add Epsilon to the stack of applications you are using and let other developers looking in the MDE direction know that you are finding it useful.</p>
+		
+	</div>
+	
+	<div id="rightcolumn">
+		
+		<div class="sideitem">
+			<h6>Spread the word</h6>
+			<p>
+If you're using Epsilon and are finding it useful, consider spreading the word using one or more of the options provided in this page. <br/><br/>As we all know, nothing motivates a developer to get into the trouble of installing and evaluating a tool better than other developers' recommendations.
+			</p>
+		</div>
+	
+	</div>
+	
+<?
+	include('stats.php');
+	$html = ob_get_contents();
+	ob_end_clean();
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
