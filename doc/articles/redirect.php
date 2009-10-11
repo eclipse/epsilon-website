3c11ae189fd616df53f69c961dc464d977fd30fe
dkolovos <dkolovos>
2009-10-11 21:34:16 +0000

    *** empty log message ***

--- doc/articles/redirect.php
+++ doc/articles/redirect.php
@@ -1,13 +1,13 @@
 <?
 function redirect() {
 	$serverName = $_SERVER['SERVER_NAME'];
-	$serverName = "www.eclipse.org";
+	//$serverName = "www.eclipse.org";
 	$requestUrl = $_SERVER["REQUEST_URI"];
 	$requestUrl = trim($requestUrl,"/");
 	$articleId = substr($requestUrl, strrpos($requestUrl,"/")+1);
 	$redirectUrl = "http://".$serverName."/gmt/epsilon/doc/articles/article.php?articleId=".$articleId;
 	echo file_get_contents($redirectUrl);
-	echo "v4";
+	echo "v5";
 	//echo "<b>Under maintenance. Please check back soon</b>";
 	//echo "<br>";
 	//echo $redirectUrl;
