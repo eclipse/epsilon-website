e055138504a8aa3548e80e0c744ea2ae934d1d03
dkolovos <dkolovos>
2010-04-21 14:33:20 +0000

    *** empty log message ***

--- download/index.php
+++ download/index.php
@@ -76,7 +76,7 @@
 			<br><b><font color="red">Interim update site </font>:</b> <a href="http://download.eclipse.org/modeling/gmt/epsilon/interim/">http://download.eclipse.org/modeling/gmt/epsilon/interim/</a> 
 			<?if (count($fixedbugs) > 0){?>
 			<br/><br/><div class="warningbox">
-			<b>Note:</b> The interim release fixes <?=count($fixedbugs)?> bug(s) (<?=$buglinks?>) that have been reported after the last stable version was released.
+			<b>Note:</b> The interim release contains fixes for <?=count($fixedbugs)?> bug(s) (<?=$buglinks?>) that have been reported after the last stable version was released.
 			</div>
 			<?}?>
 			<!--
