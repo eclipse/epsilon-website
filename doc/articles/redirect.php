c634de9d050c5cb9ac102d6471959bffabdc72fa
agarciad <agarciad>
2011-05-27 13:50:29 +0000

    redirect.php: get it working with non-default HTTP ports

--- doc/articles/redirect.php
+++ doc/articles/redirect.php
@@ -3,7 +3,11 @@ function redirect() {
 	$serverName = $_SERVER['SERVER_NAME'];
 	if ($serverName == "eclipse.org") {
 		//FIX if server == eclipse.org it doesn't work - for some reason
-		$serverName = $serverName = "www.eclipse.org";
+		$serverName = "www.eclipse.org";
+	}
+	if ($_SERVER['SERVER_PORT'] != 80) {
+		// Useful if we're working locally on the website, with a custom port number
+		$serverName = "$serverName:" . $_SERVER['SERVER_PORT'];
 	}
 	$requestUrl = $_SERVER["REQUEST_URI"];
 	$requestUrl = trim($requestUrl,"/");
