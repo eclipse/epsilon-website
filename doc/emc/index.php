40a3bf32f8d6b52f2648de097851f315f27f2c92
dkolovos <dkolovos@cs.york.ac.uk>
2012-12-26 23:41:43 +0000

    Added Epsilon architecture diagram to EMC doc page

--- doc/emc/index.php
+++ doc/emc/index.php
@@ -11,7 +11,7 @@
 		<div class="row">
 			<div class="span8">
 				<p>The vast majority of examples in this website demonstrate using  languages from Epsilon to manage EMF-based models. While Epsilon provides robust support for EMF models, it is not tied to EMF at all. In fact, Epsilon is underpined by an open model connectivity framework which developers can extend with support for additional types of models/modeling technologies by providing respective drivers. </p>
-		
+				<img src="../epsilon-architecture.png" style="padding-top:10px"/>		
 				<p>For example, in EpsilonLabs, drivers are provided for managing <a href="http://epsilonlabs.wiki.sourceforge.net/MDR+driver">MDR models</a> and <a href="http://epsilonlabs.wiki.sourceforge.net/Z+driver">Z specifications</a> (which cannot be hosted in the Eclipse SVN due to licencing issues). As most people use the EMF driver, there is not much documentation about the other drivers. However, if you're interested in using/extending them (or even providing new drivers for other modeling technologies), we'll be more than happy to help if you let us know through the <a href="../../forum">forum</a>.</p>
 			</div>
 
