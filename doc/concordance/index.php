19cbe1f44d7a1a451d50b5d8660d6b9985d4fb72
dkolovos <dkolovos@cs.york.ac.uk>
2013-12-14 14:12:55 +0000

    Updated Concordance documentation page in response to bug #414683

--- doc/concordance/index.php
+++ doc/concordance/index.php
@@ -10,9 +10,8 @@
 
 		<div class="row">
 			<div class="span8">
-				<img src="http://dev.eclipse.org/huge_icons/categories/applications-development.png" alt="" class="pull-right">
-				<p>Concordance is a tool that monitors selected projects of the workspace and maintains an index of cross-resource EMF references. Concordance can then use this index to automatically reconcile references when models are moved, and report broken references when models are updated/deleted.</p>
-		<p>Concordance is the newest part of Epsilon and its API is still volatile. We plan to have a first stable version of the API accompanied by exemplar tools in early 2010. Check back soon or drop by the <a href="../../forum">forum</a> for more details.</p>
+				<p>Concordance is a tool that monitors selected projects of the workspace and maintains an index of cross-resource EMF references. Concordance can then use this index to automatically reconcile references when models are moved, and report broken references when models are updated/deleted.
+				For a detailed presentation of Concordance, please refer to <a href="http://link.springer.com/chapter/10.1007%2F978-3-642-13595-8_20">this paper</a>.</p>
 			</div>
 		</div>
 
