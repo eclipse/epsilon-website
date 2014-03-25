244f22495eb9b51ef480379ffe007dd90e1e7dfe
dkolovos <dkolovos@cs.york.ac.uk>
2014-03-25 00:04:43 +0000

    Added BigMDE ad

--- index.php
+++ index.php
@@ -24,6 +24,18 @@
 	h('', $styles);
 ?>
 
+<div class="row">
+	<div class="span12">
+	<div class="alert alert-info" style1="background-color: rgb(116, 67, 140)">
+		<b>Upcoming events</b><button type="button" class="close" data-dismiss="alert" style="font-size:16px">x</button>   
+    <br/>
+    <p style="padding-top:10px">
+    	2<sup>nd</sup> <a href="http://www.big-mde.eu">Workshop on Scalable Model Driven Engineering</a> (BigMDE'14), July 24, 2014, York, UK
+	</p>
+    </div>
+    </div>
+</div>
+
 <!--div class="row">
 	<div class="span12">
 	<div class="alert alert-info" style1="background-color: rgb(116, 67, 140)">
