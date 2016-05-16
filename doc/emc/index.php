a7fc2718d74951fcd9c04ceae87a89a25c36681d
dkolovos <dkolovos@cs.york.ac.uk>
2016-05-16 15:06:39 +0100

    Updated EMC page

--- doc/emc/index.php
+++ doc/emc/index.php
@@ -11,8 +11,18 @@
 		<div class="row">
 			<div class="span8">
 				<p>The vast majority of examples in this website demonstrate using  languages from Epsilon to manage EMF-based models. While Epsilon provides robust support for EMF models, it is not tied to EMF at all. In fact, Epsilon is underpined by an open model connectivity framework which developers can extend with support for additional types of models/modeling technologies by providing respective drivers. </p>
-				<img src="../epsilon-architecture.png" style="padding-top:10px"/>		
-				<p>For example, in EpsilonLabs, drivers are provided for managing <a href="http://epsilonlabs.wiki.sourceforge.net/MDR+driver">MDR models</a> and <a href="http://epsilonlabs.wiki.sourceforge.net/Z+driver">Z specifications</a> (which cannot be hosted in Epsilon's Eclipse Git repository due to licencing issues). As most people use the EMF driver, there is not much documentation about the other drivers. However, if you're interested in using/extending them (or even providing new drivers for other modeling technologies), we'll be more than happy to help if you let us know through the <a href="../../forum">forum</a>.</p>
+				<img src="../epsilon-architecture.png" style="padding-top:10px"/>
+				<p>For example, in <a href="/epsilon/labs">EpsilonLabs</a>, drivers are provided for managing:
+				<ul>
+					<li><a href="https://github.com/epsilonlabs/emc-argouml">ArgoUML models</a>
+					<li><a href="https://github.com/epsilonlabs/emc-cdo">Models stored in CDO repositories</a>
+					<li><a href="https://github.com/epsilonlabs/emc-json">JSON documents</a>
+					<li><a href="https://github.com/epsilonlabs/emc-html">HTML documents</a>
+					<li><a href="https://github.com/epsilonlabs/emc-mongodb">Data stored in MongoDB databases</a>
+					<li><a href="https://github.com/epsilonlabs/emc-jdbc">Data stored in MySQL databases</a>
+				</ul>
+
+				As most Epsilon users work with EMF and XML-based models, there is not much documentation about the other drivers. However, if you're interested in using/extending them (or even providing new drivers for other modeling technologies), we'll be more than happy to help if you let us know through the <a href="../../forum">forum</a>.</p>
 			</div>
 
 		</div>
@@ -33,19 +43,19 @@
 		<h3>Reference</h3>
 		<div class="row">
 			<div class="span8">
-				<p>Chapter 3 of the <a href="../book">Epsilon book</a> provides a complete 
+				<p>Chapter 3 of the <a href="../book">Epsilon book</a> provides a complete
 		reference of the EMC.</p>
 			</div>
 		</div>
 
-	</div>	
+	</div>
 	<!-- end main part -->
 
 	<!-- sidebar -->
 	<div class="span4">
-		
+
 		<?= toolsSideItem() ?>
-		
+
 	</div>
 	<!-- end sidebar -->
 </div>
