79a3b0423b169cb273d7d39f8d2aade323622b19
dkolovos <dkolovos@cs.york.ac.uk>
2016-10-11 16:32:20 +0100

    Fixed tweet feed size

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
   require_once('template.php');
   require_once('util.php');
-  
+
   //parse FAQs
   $components = simplexml_load_file("data/components.xml")->children();
   $reasons = simplexml_load_file("data/whyepsilon.xml")->children();
@@ -27,7 +27,7 @@
 <!--div class="row">
 	<div class="span12">
 	<div class="alert alert-info" style1="background-color: rgb(116, 67, 140)">
-		<b>Upcoming events</b><button type="button" class="close" data-dismiss="alert" style="font-size:16px">x</button>   
+		<b>Upcoming events</b><button type="button" class="close" data-dismiss="alert" style="font-size:16px">x</button>
     <br/>
     <p style="padding-top:10px">
     	2<sup>nd</sup> <a href="http://www.big-mde.eu">Workshop on Scalable Model Driven Engineering</a> (BigMDE'14), July 24, 2014, York, UK
@@ -39,7 +39,7 @@
 <!--div class="row">
 	<div class="span12">
 	<div class="alert alert-info" style1="background-color: rgb(116, 67, 140)">
-		<b>We're hiring!</b><button type="button" class="close" data-dismiss="alert" style="font-size:16px">x</button>   
+		<b>We're hiring!</b><button type="button" class="close" data-dismiss="alert" style="font-size:16px">x</button>
     <br/>
     <p style="padding-top:10px">
     	Two 30-month research posts on scalable Model Driven Engineering are now open at the University of York.
@@ -93,14 +93,14 @@
   </div>
 </div>
 
-<!--span style="float:right;position:relative;top:20px"><script id='fbwyf51'>(function(i){var f,s=document.getElementById(i);f=document.createElement('iframe');f.src='//api.flattr.com/button/view/?uid=epsilonews&button=compact&url=http%3A%2F%2Fwww.eclipse.org%2Fepsilon';f.title='Flattr';f.height=20;f.width=110;f.style.borderWidth=0;s.parentNode.insertBefore(f,s);})('fbwyf51');</script></span-->  
+<!--span style="float:right;position:relative;top:20px"><script id='fbwyf51'>(function(i){var f,s=document.getElementById(i);f=document.createElement('iframe');f.src='//api.flattr.com/button/view/?uid=epsilonews&button=compact&url=http%3A%2F%2Fwww.eclipse.org%2Fepsilon';f.title='Flattr';f.height=20;f.width=110;f.style.borderWidth=0;s.parentNode.insertBefore(f,s);})('fbwyf51');</script></span-->
 <div class="tabbable" style="margin-bottom: 0px;">
   <ul class="nav nav-tabs">
     <li><a href="#languages" data-toggle="tab"><h4>Languages</h4></a></li>
     <li><a href="#tools" data-toggle="tab"><h4>Tools</h4></a></li>
     <li class="active"><a href="#whyepsilon" data-toggle="tab"><h4>Why Epsilon?</h4></a></li>
-    <li><a href="#twitter" data-toggle="tab"><h4>@epsilonews</h4></a></li>
-    
+    <li><a href="#twitter" data-toggle="tab"><h4>News</h4></a></li>
+
     <!--
     <a style="position:relative;top:19px;left:521px" href="http://marketplace.eclipse.org/marketplace-client-intro?mpc_install=400" title="Drag and drop into a running Eclipse Indigo workspace to install Epsilon">
 	  <img src="/epsilon/img/installbutton.png"/>
@@ -128,13 +128,13 @@
     <?}}?>
   	</div>
   <?}?>
-  
+
   <div class="tab-pane active" id="whyepsilon">
   <?$i=0;?>
   <?foreach ($reasons as $reason){?>
   <?if($i%2==0){?><div class="row" style="padding-bottom:20px"><?}?>
   	<div class="span1">
-  		<img style="position:relative;top:10px;left:10px" src="img/stylistica/<?=$reason["icon"]?>.png"> 
+  		<img style="position:relative;top:10px;left:10px" src="img/stylistica/<?=$reason["icon"]?>.png">
   	</div>
   	<div class="span5">
   		<h5><?=$reason["title"]?>.</h5><p><?=$reason?></p>
@@ -145,9 +145,9 @@
   <p style="text-align:right">* Icons by <a href="http://dryicons.com/">http://dryicons.com</a></p>
   </div>
 
-    
+
   <div class="tab-pane" id="twitter">
-  <a class="twitter-timeline" href="https://twitter.com/epsilonews" data-widget-id="308297319382654976" width="700">Tweets by @epsilonews</a>
+  <a class="twitter-timeline" href="https://twitter.com/epsilonews" data-widget-id="308297319382654976" width="400">Tweets by @epsilonews</a>
 	<script>window.twttr=(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],t=window.twttr||{};if(d.getElementById(id))return;js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);t._e=[];t.ready=function(f){t._e.push(f);};return t;}(document,"script","twitter-wjs"));</script>
   </div>
 </div>
@@ -161,7 +161,7 @@
 	);
 
 	f($scripts);
-	
+
 
 ?>
 
