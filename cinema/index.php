0c767c2ca5fdfe137bdb6171057a783ab6d085f0
dkolovos <dkolovos@cs.york.ac.uk>
2017-07-03 16:14:20 +0100

    Updated YouTube URL

--- cinema/index.php
+++ cinema/index.php
@@ -1,3 +1,3 @@
 <?php
-header( 'Location: https://www.youtube.com/channel/UCn9rS_c10R4Tww39xkTVEIw' ) ;
+header( 'Location: https://www.youtube.com/channel/UCn9rS_c10R4Tww39xkTVEIw/videos?view=0&sort=dd&shelf_id=0' ) ;
 ?>
