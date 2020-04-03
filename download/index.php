cd42952aa7650fb57f3707e4c7aa43db2432112c
Sina Madani <sinadoom@googlemail.com>
2020-04-03 02:47:14 +0100

    'top left' -> 'top right' under troubleshoot instructions

--- download/index.php
+++ download/index.php
@@ -151,7 +151,7 @@
 							<p>
 							Download the <a href="https://www.eclipse.org/downloads/packages/installer"><b>Eclipse Installer</b></a> and select Epsilon. Note that you will need a <a href="https://adoptopenjdk.net/">Java Runtime Environment</a> installed on your system.
 							</p><br/><p>
-							If you don't see Epsilon in the list, click on the three vertical bars on the top left, then <i>Product Catalogs...</i> and check <i>Eclipse.org Applications</i>. Epsilon should be near the bottom of the list. If the <i>Product Catalogs...</i> option is missing, switch to <i>Advanced Mode</i> and look for Epsilon under <i>Eclipse.org Applications</i>. Press <i>Next</i> 3 times (you don't need to change anything) and then <i>Finish</i>. Accept any licenses when prompted.
+							If you don't see Epsilon in the list, click on the three vertical bars on the top right, then <i>Product Catalogs</i> and check "Eclipse.org Applications". Epsilon should be near the bottom of the list (you can also use the search bar). If the <i>Product Catalogs</i> option is missing, switch to <i>Advanced Mode</i> and look for Epsilon under <i>Eclipse.org Applications</i>. Keep pressing <i>Next</i> (you don't need to change anything) and then <i>Finish</i>. Accept the licenses when prompted.
 							</p>
 						</div>
 				    	<?if($distributions){?>
