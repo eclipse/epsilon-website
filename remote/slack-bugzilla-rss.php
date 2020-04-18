e0cc45a0b35d62f7fb9ffaf45f32b76fe4186db5
Dimitris Kolovos <dkolovos@cs.york.ac.uk>
2020-04-18 20:25:14 +0100

    Updated Slack RSS source URL

--- remote/slack-bugzilla-rss.php
+++ remote/slack-bugzilla-rss.php
@@ -1,6 +1,6 @@
 <?
 header('Content-Type: text/xml');
-$url = "https://bugs.eclipse.org/bugs/buglist.cgi?component=Core&product=Epsilon&query_format=advanced&resolution=---&title=Bug%20List&ctype=atom";
+$url = "https://bugs.eclipse.org/bugs/buglist.cgi?bug_severity=blocker&bug_severity=critical&bug_severity=major&bug_severity=normal&bug_severity=minor&bug_severity=trivial&bug_status=UNCONFIRMED&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&bug_status=RESOLVED&list_id=19426194&product=epsilon&query_format=advanced&title=Bug%20List&ctype=atom";
 $xml = simplexml_load_file($url);
 foreach($xml->entry as $entry) {
 	$entry->summary = "";
