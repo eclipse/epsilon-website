f1bafff9d39351ede42e4f999850422bc5c510ad
Sina Madani <sinadoom@googlemail.com>
2020-06-01 17:50:52 +0100

    Don't show jars if latest

--- download/index.php
+++ download/index.php
@@ -49,7 +49,7 @@
 	}
 	
 	$distributions = (strcmp($major_release["distributions"], "no") != 0);
-	$jars = (strcmp($release["jars"], "yes") == 0);
+	$jars = (!$latest) and (strcmp($release["jars"], "yes") == 0);
 
 	$downloadUrl = "https://www.eclipse.org/downloads/download.php?file=/epsilon/".$major_release_bread_crumb."distributions/eclipse-epsilon-".$major_release["version"]."-";
 	$downloadWin64 = $downloadUrl."win32-x86_64.zip";
