c7a87b9fcb87bbecef87988a338400a6f84bd8f4
Dimitris Kolovos <dkolovos@cs.york.ac.uk>
2021-02-17 11:18:02 +0000

    Removed dots from carousel
    Signed-off-by: Dimitris Kolovos <dkolovos@cs.york.ac.uk>

--- mkdocs/docs/assets/javascript/extra.js
+++ mkdocs/docs/assets/javascript/extra.js
@@ -2,8 +2,12 @@ $(document).ready(function(){
   $('.carousel').slick({
   	arrows: false,
 	fade: true,
-	dots: true,
+	dots: false,
 	adaptiveHeight: true,
-	lazyLoad: 'ondemand'
+	lazyLoad: 'ondemand',
+	centerMode: true,
+	autoplay: true,
+  	autoplaySpeed: 5000,
+  	infinite: true
   });
 });
