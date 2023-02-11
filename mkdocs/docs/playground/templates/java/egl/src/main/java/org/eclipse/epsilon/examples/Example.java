1cdc5560f46404c05f45e25392df63644da75209
Dimitris Kolovos <dimitris.kolovos@york.ac.uk>
2023-02-11 10:40:53 +0000

    Added EGL Java example

--- mkdocs/docs/playground/templates/java/egl/src/main/java/org/eclipse/epsilon/examples/Example.java
+++ mkdocs/docs/playground/templates/java/egl/src/main/java/org/eclipse/epsilon/examples/Example.java
@@ -4,8 +4,8 @@ import java.io.File;
 
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.emfatic.core.EmfaticResourceFactory;
+import org.eclipse.epsilon.egl.EglTemplateFactoryModuleAdapter;
 import org.eclipse.epsilon.emc.emf.EmfModel;
-import org.eclipse.epsilon.epl.EplModule;
 import org.eclipse.epsilon.flexmi.FlexmiResourceFactory;
 
 public class Example {
@@ -16,9 +16,9 @@ public class Example {
         Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("flexmi", new FlexmiResourceFactory());
         Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("emf", new EmfaticResourceFactory());
         
-        // Parse the EPL pattern matching rules
-        EplModule module = new EplModule();
-        module.parse(new File("program.epl"));
+        // Parse the EGL template
+        EglTemplateFactoryModuleAdapter module = new EglTemplateFactoryModuleAdapter();
+        module.parse(new File("program.egl"));
         
         // Load the model from model.flexmi using metamodel.emf as its metamodel
         EmfModel model = new EmfModel();
@@ -29,21 +29,14 @@ public class Example {
         model.setStoredOnDisposal(false);
         model.load();
         
-        // Make the model available to the program
+        // Make the model available to the template
         module.getContext().getModelRepository().addModel(model);
         
-        // Execute the EPL pattern matching rules
-        module.execute();
+        // Execute the EGL template
+        String generated = (String) module.execute();
 
-        // Print the patterns found
-        module.getContext().getPatternMatchTrace().getMatches().forEach(match -> {
-            // Print the name of the pattern
-            System.out.println(match.getPattern().getName());
-            // Print the model elements involved in the match
-            match.getRoleBindings().forEach((key, value) -> {
-                System.out.println("\t" + key + " = " + value);
-            });
-        });
+        // Print the generated text
+        System.out.println(generated);
 
         // Dispose of the model
         module.getContext().getModelRepository().dispose();
