52b9099436aa70db4aaa72258f04d98a9665c414
Dimitris Kolovos <dimitris.kolovos@york.ac.uk>
2023-02-12 12:04:10 +0000

    Consolidated EML into Maven/Gradle templates

--- mkdocs/docs/playground/js/DownloadDialog.js
+++ mkdocs/docs/playground/js/DownloadDialog.js
@@ -46,16 +46,17 @@ class DownloadDialog {
                             etl: language == "etl",
                             flock: language == "flock",
                             etlOrFlock: language == "etl" || language == "flock",
-                            egl: language == "egl"
+                            egl: language == "egl",
+                            eml: language == "eml"
                         };
     
                         if (format == "gradle") {
-                            var template = Handlebars.compile(self.fetchTemplate(language == "eml" ? "build-eml.gradle" : "build.gradle.handlebars"));
+                            var template = Handlebars.compile(self.fetchTemplate("build.gradle.handlebars"));
                             zip.file("build.gradle", template(templateData));
                             zip.file("readme.md", self.fetchTemplate("readme-gradle.txt"));
                         }
                         else if (format == "maven") {
-                            var template = Handlebars.compile(self.fetchTemplate(language == "eml" ? "pom-eml.xml" : "pom.xml.handlebars"));
+                            var template = Handlebars.compile(self.fetchTemplate("pom.xml.handlebars"));
                             zip.file("pom.xml", template(templateData));
                             zip.file("readme.md", self.fetchTemplate("readme-maven.txt"));
                         }
