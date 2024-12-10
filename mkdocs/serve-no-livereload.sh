9bb1996351e022e70bc5b444bf6e8711d49e7bad
Antonio Garcia-Dominguez <a.garcia-dominguez@york.ac.uk>
2024-12-10 09:38:44 +0000

    CI: only use virtualenv if it is installed (helps with caching)

--- mkdocs/serve-no-livereload.sh
+++ mkdocs/serve-no-livereload.sh
@@ -5,12 +5,15 @@ set -e
 # Go to the directory with this script
 cd "$(dirname "$(readlink -f "$0")")"
 
-# Create a Python virtualenv if it does not exist yet
-if ! test -d env; then
-  virtualenv -p python3 env
+# Create a Python virtualenv if we have the tool installed
+# and it does not exist yet.
+if which virtualenv; then
+  if ! test -d env; then
+    virtualenv -p python3 env
+  fi
+  source env/bin/activate
 fi
 
 # Serve the website without live reloading (useful for tests)
-source env/bin/activate
 pip install -r requirements.txt
 mkdocs serve --no-livereload
